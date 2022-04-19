903fd24e17e50b3f2e66f5bbc76632fb1d82554e
Sohep Alslamat <sohep.alslamat91@gmail.com>
2022-04-20 01:13:13 +0300

    add basename to router

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     return (
         <div className="App">
             <NavBar />
-            <Router>
+            <Router basename="/corona-virus-reactjs">
                 <ActinosBar />
                 <Routes>
                     <Route path="/home" element={<Home />} />
